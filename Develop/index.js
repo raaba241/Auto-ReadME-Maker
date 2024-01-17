1c43102597a806380527711d9c2d90cb0121b592
unknown <raaba21@gmail.com>
2024-01-17 00:57:30 -0500

    added tests to check what my answers are

--- Develop/index.js
+++ Develop/index.js
@@ -61,7 +61,11 @@ function writeToFile(fileName, data) {
 }
 
 // TODO: Create a function to initialize app
-function init() { }
+function init() {
+    inquirer.prompt(questions).then((answers) => {
+        console.log (answers)
+    })
+}
 
 // Function call to initialize app
 init();
