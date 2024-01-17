65d2a3d9454e12f6d42b13b1153cc2fe5bf091d8
unknown <raaba21@gmail.com>
2024-01-16 23:29:44 -0500

    added questions array

--- Develop/index.js
+++ Develop/index.js
@@ -1,7 +1,41 @@
 // TODO: Include packages needed for this application
+const inquirer = require('inquirer')
 
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = [
+    {
+        type: 'input',
+        name: 'title',
+        message: 'Enter a title for this project'
+    },
+    {
+        type: 'input',
+        name: 'description',
+        message: 'Enter a short description for the project'
+    },
+    {
+        type: 'input',
+        name: 'isntallation instructions',
+        message: 'Enter installation instructions'
+    },
+    {
+        type: 'input',
+        name: 'usage guidelines',
+        message: 'Enter the usage guidlines for this project'
+    },
+    {
+        type: 'input',
+        name: 'test instrucitons',
+        message: 'Enter instructions for testing'
+    }
+    {
+        type: 'list',
+        name: 'license',
+        message: 'Pick a license for this project',
+        choices: ['MIT',]
+
+    }
+];
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {}
